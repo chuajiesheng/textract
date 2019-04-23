9d74475ec0f3ad533fab0f95c0f23f3912b76dcf
Chua Jie Sheng <chuajiesheng@gmail.com>
2019-04-24 00:19:20 +0800

    Add large pdf into the fixture

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -28,12 +28,7 @@ class PdfTestCase(base.ShellParserTestCase, unittest.TestCase):
     def test_large_pdf(self):
         """Make sure extraction does not hang (issue #33)"""
 
-        # download the file
         filename = os.path.join(self.get_extension_directory(), "large.pdf")
-        self.download_file(
-            "https://openknowledge.worldbank.org/bitstream/handle/10986/16091/9780821399378.pdf",
-            filename,
-        )
 
         # make sure textract can successfully run
         self.assertSuccessfulTextract(filename)
